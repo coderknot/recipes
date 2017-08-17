40bb3f2fe4658127364f9da322ae363699e2eadd
Chris Carr <coder.knot@gmail.com>
2017-08-17 11:22:41 -0700

    navigate to edit recipe component on button click

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
   index: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -28,6 +29,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    // this.router.navigate(['../', this.index, 'edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
